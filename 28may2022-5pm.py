6246b2c4bd323a85012011075e548d0f2cdc2b66
EricNk <eric.nkamgnia@.yahoo.ca>
2022-05-29 23:40:12 +0000

    this is my third commit

--- 28may2022-5pm.py
+++ 28may2022-5pm.py
@@ -1,5 +1,5 @@
 thisset = {"apple", "banana", "cherry"} 
 del thisset 
 print(thisset) #this will raise an error because the set no longer exists 
-thisset = {"cherry","mango"}
-
+thisset = {"cherry","mango"} 
+print "This is a great day"
