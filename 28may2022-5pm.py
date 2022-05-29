3d524c8abfaf3b7cc46c479a2270abcc41525740
EricNk <105750343+EricNk@users.noreply.github.com>
2022-05-29 19:58:58 -0400

    second change from remote repo directly

--- 28may2022-5pm.py
+++ 28may2022-5pm.py
@@ -3,4 +3,4 @@ del thisset
 print(thisset) #this will raise an error because the set no longer exists 
 thisset = {"cherry","mango"} 
 print "This is a great day"
-print "This is a great day that I have added from the remote repo"
+print "This is a great day that I have added from the remote repo at 8pm on may29-2022"
