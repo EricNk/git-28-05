16eacba0ab905aef2f883aa859330b2dc9b071fc
EricNk <eric.nkamgnia@.yahoo.ca>
2022-05-30 03:15:42 +0000

    Change from the devops branch

--- 28may2022-5pm.py
+++ 28may2022-5pm.py
@@ -4,3 +4,4 @@ print(thisset) #this will raise an error because the set no longer exists
 thisset = {"cherry","mango"} 
 print "This is a great day"
 print "This is a great day that I have added from the remote repo. I added this from local repo"
+print "This is the new branch devops"
